a00dbb3ec19deb2d6f8760aa012150ecaaeae15b
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2021-02-28 12:09:53 +0100

    add service worker

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite"
 import preactRefresh from "@prefresh/vite"
 import WindiCSS from "vite-plugin-windicss"
+import { VitePWA } from "vite-plugin-pwa"
+import { readFileSync } from "fs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +17,13 @@ export default defineConfig({
     jsxFragment: "Fragment",
     // jsxInject: `import { h, Fragment } from 'preact'`,
   },
-  plugins: [WindiCSS(), preactRefresh()],
+  plugins: [
+    WindiCSS(),
+    preactRefresh(),
+    VitePWA({
+      manifest: JSON.parse(
+        readFileSync("./public/manifest.webmanifest", "utf-8")
+      ),
+    }),
+  ],
 })
